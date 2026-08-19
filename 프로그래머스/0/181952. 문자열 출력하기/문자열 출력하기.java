88a38350ce3e79de44371130d017a45ad15e26b1
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2026-08-19 22:34:25 +0900

    [level 0] Title: 문자열 출력하기, Time: 78.53 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String a=br.readLine();
+        System.out.print(a);
+    }
+}
