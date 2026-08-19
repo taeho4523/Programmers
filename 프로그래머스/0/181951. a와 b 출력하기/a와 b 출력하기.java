f55669af793d6f0e30025da4774966fc316bb229
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2026-08-19 22:38:54 +0900

    [level 0] Title: a와 b 출력하기, Time: 111.01 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println("a = " + st.nextToken());
+        System.out.print("b = " + st.nextToken());
+        
+    }
+}
